c5e2c5786c67eb6de07f598107b3a9379a2e4097
Dan Sorenson <dsore20@wgu.edu>
2025-11-18 20:43:59 -0700

    Implement database connection
    This commit uses a JDBC API to establish a database connection and SQL operations. This is achieved using H2, which creates a database at runtime.

--- src/main/java/com/github/sdan20/runnerz/run/RunRepository.java
+++ src/main/java/com/github/sdan20/runnerz/run/RunRepository.java
@@ -1,60 +1,77 @@
 package com.github.sdan20.runnerz.run;
 
-import java.time.LocalDateTime;
-import java.time.temporal.ChronoUnit;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
-
-import jakarta.annotation.PostConstruct;
+import org.springframework.util.Assert;
 
 @Repository
 public class RunRepository {
 
-    private List<Run> runs = new ArrayList<>();
+    private final JdbcClient jdbcClient;
 
-    List<Run> findAll() {
-        return runs;
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
     }
 
-    Optional<Run> findById(Integer id) {
-        return runs.stream()
-                .filter(run -> run.id() == id)
-                .findFirst();
+    public List<Run> findAll() {
+        return jdbcClient.sql("SELECT * FROM RUN")
+                .query(Run.class)
+                .list();
     }
 
-    void create(Run run) {
-        runs.add(run);
+    public Optional<Run> findById(Integer id) {
+        return jdbcClient.sql("SELECT id, title, started_one, completed_on, miles, location FROM RUN WHERE id = :id")
+                .param("id", id)
+                .query(Run.class)
+                .optional();
     }
 
-    void update(Run run, Integer id) {
-        Optional<Run> existingRun = findById(id);
-        if(existingRun.isPresent()) {
-            runs.set(runs.indexOf(existingRun.get()), run);
-        }
+    public void create(Run run) {
+        var updated = jdbcClient
+                .sql("INSERT INTO RUN (id, title, started_on, completed_on, miles, location) VALUES (?,?,?,?,?,?)")
+                .params(List.of(run.id(), run.title(), run.startedOn(), run.completedOn(), run.miles(),
+                        run.location().toString()))
+                .update();
+
+        Assert.state(updated == 1, "Failed to create run " + run.title());
     }
 
-    void delete(Integer id) {
-        runs.removeIf(run -> run.id().equals(id));
+    public void update(Run run, Integer id) {
+        var updated = jdbcClient.sql(
+                "UPDATE RUN SET title = ?, started_one = ?, completed_on = ?, miles = ?, location = ? WHERE id = ?")
+                .params(List.of(run.title(), run.startedOn(), run.completedOn(), run.miles(),
+                        run.location().toString(), id))
+                .update();
+
+        Assert.state(updated == 1, "Failed to update run " + run.title());
     }
 
-    @PostConstruct
-    private void init() {
-        runs.add(new Run(1,
-                "Monday Morning Run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(35, ChronoUnit.MINUTES),
-                3,
-                Location.INDOOR));
+    public void delete(Integer id) {
+        var updated = jdbcClient.sql("DELETE FROM RUN WHERE id = :id")
+                .param("id", id)
+                .update();
 
-        runs.add(new Run(2,
-                "Wednesday Evening Run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(55, ChronoUnit.MINUTES),
-                5,
-                Location.OUTDOOR));
+        Assert.state(updated == 1, "Failed to delete run " + id);
     }
 
+    public int count() {
+        return jdbcClient.sql("SELECT * FROM RUN")
+                .query()
+                .listOfRows()
+                .size();
+    }
+
+    public void saveAll(List<Run> runs) {
+        runs.stream().forEach(this::create);
+    }
+
+    public List<Run> findByLocation(String location) {
+        return jdbcClient.sql("SELECT * FROM RUN WHERE LOCATION = :location")
+                .param("location", location)
+                .query(Run.class)
+                .list();
+    }
 }
---
+++ src/main/java/com/github/sdan20/runnerz/run/Runs.java
@@ -0,0 +1,6 @@
+package com.github.sdan20.runnerz.run;
+
+import java.util.List;
+
+public record Runs(List<Run> runs) {
+}
