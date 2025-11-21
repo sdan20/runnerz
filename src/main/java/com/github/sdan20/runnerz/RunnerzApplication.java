90fbee8628773cc0b957879ad586df46cbfddd28
Dan Sorenson <dsore20@wgu.edu>
2025-11-21 07:31:58 -0700

    Create testing functionality
    This commit implements a UserHttpClient, which calls more abstract methods from Spring Boot. It also adds local database credentials in application.properties, so that tests can run without the use of a separate test container.

--- src/main/java/com/github/sdan20/runnerz/RunnerzApplication.java
+++ src/main/java/com/github/sdan20/runnerz/RunnerzApplication.java
@@ -6,8 +6,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestClient;
+import org.springframework.web.client.support.RestClientAdapter;
+import org.springframework.web.service.invoker.HttpServiceProxyFactory;
 
 import com.github.sdan20.runnerz.user.User;
+import com.github.sdan20.runnerz.user.UserHttpClient;
 import com.github.sdan20.runnerz.user.UserRestClient;
 
 @SpringBootApplication
@@ -18,7 +22,14 @@ public class RunnerzApplication {
 	}
 
 	@Bean
-	CommandLineRunner runner(UserRestClient client) {
+	UserHttpClient userHttpClient() {
+		RestClient restClient = RestClient.create("https://jsonplaceholder.typicode.com/");
+		HttpServiceProxyFactory factory = HttpServiceProxyFactory.builderFor(RestClientAdapter.create(restClient)).build();
+		return factory.createClient(UserHttpClient.class);
+	}
+
+	@Bean
+	CommandLineRunner runner(UserHttpClient client) {
 		return args -> {
 
 			List<User> users = client.findAll();
